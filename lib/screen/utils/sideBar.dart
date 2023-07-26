6e2b5c52dd78fa84ae017ecff264cfb97e864d94
Francesco è volevi <deadragon99@outlook.it>
2023-07-26 11:58:14 +0200

    fix logout button

--- lib/screen/utils/sideBar.dart
+++ lib/screen/utils/sideBar.dart
@@ -99,7 +99,6 @@ class _sideBarState extends State<sideBar> {
                 ],
               ),
             ),
-
             GestureDetector(
               onTap: () {
                 Navigator.pushNamed(context, 'home');
